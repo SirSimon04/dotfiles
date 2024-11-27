38b2894c189aece1fb71fba8f4949f8ddfbeea10
Simon Engel <simon.engel01@sap.de>
2024-11-27 14:20:59 +0100

    Update vim bindings for noted

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -24,7 +24,6 @@ return {
     -- Add your own debuggers here
     -- 'leoluz/nvim-dap-go',
     'mxsdev/nvim-dap-vscode-js',
-    'mfussenegger/nvim-jdtls',
   },
   config = function()
     local dap = require 'dap'
