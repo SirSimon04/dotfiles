b95a49f1ae58fd9b54115e10251eb341dc6fdc87
Simon Engel <simon.engel01@sap.de>
2024-12-02 16:11:36 +0100

    Performance Updates

--- .config/nvim/lua/custom/plugins/lazygit.lua
+++ .config/nvim/lua/custom/plugins/lazygit.lua
@@ -1,23 +1,14 @@
 return {
   'kdheepak/lazygit.nvim',
-  config = function()
-    require('lazy').setup {
-      {
-        'kdheepak/lazygit.nvim',
-        cmd = {
-          'LazyGit',
-          'LazyGitConfig',
-          'LazyGitCurrentFile',
-          'LazyGitFilter',
-          'LazyGitFilterCurrentFile',
-        },
-        -- optional for floating window border decoration
-        dependencies = {
-          'nvim-lua/plenary.nvim',
-        },
-      },
-    }
-
-    vim.keymap.set('n', '<leader>gg', '<cmd>:LazyGit<cr>', { desc = 'Open Lazy[G]it' })
-  end,
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
 }
--- .config/nvim/lua/kickstart/plugins/which-key.lua
+++ .config/nvim/lua/kickstart/plugins/which-key.lua
@@ -13,22 +13,32 @@
 
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
     require('which-key').setup()
 
     -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-      ['<leader>hc'] = { name = '[C]lear', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>a'] = { name = '[A]I-Gen', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]est', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
+    require('which-key').add {
+      { '<leader>a', group = '[A]I-Gen' },
+      { '<leader>a_', hidden = true },
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>d', group = '[D]ebug' },
+      { '<leader>d_', hidden = true },
+      { '<leader>g', group = '[G]it' },
+      { '<leader>g_', hidden = true },
+      { '<leader>h', group = '[H]arpoon' },
+      { '<leader>h_', hidden = true },
+      { '<leader>hc', group = '[C]lear' },
+      { '<leader>hc_', hidden = true },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>t', group = '[T]est' },
+      { '<leader>t_', hidden = true },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>w_', hidden = true },
     }
   end,
 }
