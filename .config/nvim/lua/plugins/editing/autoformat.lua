72bb5217f1096160dad39df39646ea727b8d7ff3
Simon Engel <simon.engel01@sap.com>
2025-01-13 18:42:59 +0100

    Add disabling of format on save

--- .config/nvim/lua/plugins/editing/autoformat.lua
+++ .config/nvim/lua/plugins/editing/autoformat.lua
@@ -2,10 +2,17 @@ return { -- Autoformat
   'stevearc/conform.nvim',
   opts = {
     notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
+    -- format_on_save = {
+    --   timeout_ms = 500,
+    --   lsp_fallback = true,
+    -- },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = 'fallback' }
+    end,
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
@@ -15,6 +22,7 @@ return { -- Autoformat
       -- is found.
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
       typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      bib = { 'bibtex-tidy' },
     },
   },
 }
