ef8605105f7fcff7e6ea809325edc6ce27d586e4
SirSimon04 <simi@engelnetz.de>
2025-03-16 02:05:25 +0100

    Update AI plugins

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,24 +1,26 @@
 return {
-  {
-    'David-Kunz/gen.nvim',
-
-    config = function()
-      require('gen').setup {
-        model = 'llama3.1', -- The default model to use.
-        quit_map = 'q', -- set keymap to close the response window
-        display_mode = 'split',
-        show_prompt = true, -- Shows the prompt submitted to Ollama.
-        show_model = true, -- Displays which model you are using at the beginning of your chat session.
-      }
-
-      require('gen').prompts['Generate_test'] = {
-        prompt = 'Write a funtion that makes the following test pass. It should be in $filetype. /n$text',
-        model = 'codellama',
-      }
-    end,
-  },
+  -- {
+  --   'David-Kunz/gen.nvim',
+  --
+  --   config = function()
+  --     require('gen').setup {
+  --       model = 'llama3.1', -- The default model to use.
+  --       quit_map = 'q', -- set keymap to close the response window
+  --       display_mode = 'split',
+  --       show_prompt = true, -- Shows the prompt submitted to Ollama.
+  --       show_model = true, -- Displays which model you are using at the beginning of your chat session.
+  --     }
+  --
+  --     require('gen').prompts['Generate_test'] = {
+  --       prompt = 'Write a funtion that makes the following test pass. It should be in $filetype. /n$text',
+  --       model = 'codellama',
+  --     }
+  --   end,
+  -- },
   {
     'github/copilot.vim',
+    lazy = true,
+    event = 'BufRead',
     config = function()
       vim.keymap.set('i', '<C-x>', 'copilot#Accept("\\<CR>")', {
         expr = true,
@@ -30,9 +32,24 @@ return {
   {
     {
       'CopilotC-Nvim/CopilotChat.nvim',
+      lazy = true,
       dependencies = {
         { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
         { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+      },
+      keys = {
+        {
+          '<leader>ac',
+          '<cmd>CopilotChat<cr>',
+          mode = { 'n', 'v' },
+          desc = 'Open Copilot Chat',
+        },
+        {
+          '<leader>ap',
+          '<cmd>CopilotChatPrompts<cr>',
+          mode = { 'n', 'v' },
+          desc = 'Copilot Chat [P]rompts',
+        },
       },
       build = 'make tiktoken', -- Only on MacOS or Linux
       opts = {},
